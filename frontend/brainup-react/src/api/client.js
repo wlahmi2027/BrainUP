f4c3a73d12ff8620593921660d21ac5fe825a4f6
Wissam Lahmidi <wissam.lahmidi@isen-ouest.yncrea.fr>
2026-03-10 12:50:04 +0100

    modification de cleint.js (plus de fetch ici)

--- frontend/brainup-react/src/api/client.js
+++ frontend/brainup-react/src/api/client.js
@@ -1,6 +1,39 @@
 import axios from "axios";
 
 export const api = axios.create({
+  baseURL: "http://127.0.0.1:8001/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API error:", error?.response || error?.message || error);
+    return Promise.reject(error);
+  }
+);
+
+
+//Ancien 
+/*
+import axios from "axios";
+
+export const api = axios.create({
   baseURL: "http://127.0.0.1:8001/api"
 
 });
@@ -22,4 +55,4 @@ export async function login(email, password) {
   });
 
   return response.json();
-}
+}*/
